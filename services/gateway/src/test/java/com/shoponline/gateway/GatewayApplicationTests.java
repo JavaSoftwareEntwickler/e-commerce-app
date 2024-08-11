6390dc9b10007dffd269bc66eaa181c818ddbc93
JavaSoftwareEntwickler <mm.java.software@gmail.com>
2024-08-11 20:40:28 +0200

    Aggiunto gateway

---
+++ services/gateway/src/test/java/com/shoponline/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shoponline.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
